13ea39678e7c5f5ee9771e49de2532e74c4f5c85
Prakash <prakashchandrasahoo8991@gmail.com>
2023-07-08 00:03:49 +0530

    changes

--- 4array_string/array/foreach.js
+++ 4array_string/array/foreach.js
@@ -1,12 +1,13 @@
 /**
  * when we need to perform a same operation for each element of a array there we use for each method
  * we can pass three argument forEach(value,index,arr)
+ * 
  */
  let arr=[1,2,3,4,5,6,7,8,9];
 
  arr.forEach((ele)=>{  console.log(ele*ele)});
 
-//for....of loop using this loop we can do the iteration method
+//for....of loop using this loop we can do the iteration method only for array
 
 for(let i of arr){
     console.log(i) //[1,2,3,4,5,6,7,8,9];
