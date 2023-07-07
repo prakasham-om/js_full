8ff406d692abf7cb857b25d74554c199d5c668e2
Prakash <prakashchandrasahoo8991@gmail.com>
2023-07-07 23:41:02 +0530

    spreadop file added

---
+++ 11restoperator/spread.js
@@ -0,0 +1,13 @@
+let a=[1,2,3,4,5,6];
+
+//if we want copy the array 
+
+/* 
+  let b=a ----> it's a wrong way it not copy the arrray it refer the array and when we change
+  in somthing in b then it will change the parent array
+*/
+
+// so spread operator used to copy
+
+
+let b=[...a]
