55e2d6b5892a87438da8c52748cc1060304c0d81
Prakash <prakashchandrasahoo8991@gmail.com>
2023-07-07 23:26:39 +0530

    ececution  context

---
+++ 18execution_conext_lexicsl_env/exe_context.js
@@ -0,0 +1,7 @@
+ /**
+  * execution context is a container where the function's code is executed
+    and it create when the function is called,it contains 3 things variable,function
+    and lexical env;
+    
+    and lexical env derive us which data is accessble or not
+  */
